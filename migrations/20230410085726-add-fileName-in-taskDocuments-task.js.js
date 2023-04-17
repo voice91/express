a6c8795252835d1838125330aac41f7d4c997e24
MAC <mac@MACs-iMac-Pro.local>
2023-04-17 13:20:22 +0530

    Migration changes

--- migrations/20230410085726-add-fileName-in-taskDocuments-task.js.js
+++ migrations/20230410085726-add-fileName-in-taskDocuments-task.js.js
@@ -1,23 +1,24 @@
-const { Task } = require('../models');
+// BCS of Cannot use import statement outside a module in staging server
+// const { Task } = require('../models');
 
 module.exports = {
   async up() {
-    const taskModel = await Task.find({ taskDocuments: { $exists: true } }).lean();
-    await Promise.all(
-      taskModel.map(async (data) => {
-        const taskDocUpdate = data.taskDocuments
-          .filter((value) => value)
-          .map((item) => {
-            const splitUrl = item.split('/');
-            const urlSplit = splitUrl[splitUrl.length - 1].split('_');
-            const fileName = decodeURI(urlSplit[1]);
-            return { url: item, fileName };
-          });
-        await Task.findByIdAndUpdate(data._id, {
-          taskDocuments: taskDocUpdate,
-        });
-      })
-    );
+    // const taskModel = await Task.find({ taskDocuments: { $exists: true } }).lean();
+    // await Promise.all(
+    //   taskModel.map(async (data) => {
+    //     const taskDocUpdate = data.taskDocuments
+    //       .filter((value) => value)
+    //       .map((item) => {
+    //         const splitUrl = item.split('/');
+    //         const urlSplit = splitUrl[splitUrl.length - 1].split('_');
+    //         const fileName = decodeURI(urlSplit[1]);
+    //         return { url: item, fileName };
+    //       });
+    //     await Task.findByIdAndUpdate(data._id, {
+    //       taskDocuments: taskDocUpdate,
+    //     });
+    //   })
+    // );
   },
 
   async down() {
