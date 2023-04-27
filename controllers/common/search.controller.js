b1f0d15a58d61ea6957a9d66e73da7120c48c210
chaitali <chaitali@matlabinfotech.com>
2023-04-27 18:45:34 +0530

    Universal Search

--- controllers/common/search.controller.js
+++ controllers/common/search.controller.js
@@ -1,24 +1,28 @@
 import httpStatus from 'http-status';
 import { catchAsync } from '../../utils/catchAsync';
 import { pick } from '../../utils/pick';
-import { dealService } from '../../services';
+import { dealService, lendingInstitutionService } from '../../services';
 
 // eslint-disable-next-line import/prefer-default-export
 export const getDealsAndLendersForUniversalSearch = catchAsync(async (req, res) => {
   const { query } = req;
   const user = req.user._id;
-  const fields = pick(query, ['dealName']);
+  const fields = pick(query, ['searchValue']);
 
-  const filter = {
+  const filterForDeal = {
     $or: [
       { user },
       { 'involvedUsers.advisors': user },
       { 'involvedUsers.borrowers': user },
       { 'involvedUsers.lenders': user },
     ],
-    dealName: new RegExp(fields.dealName, 'i'),
+    dealName: new RegExp(fields.searchValue, 'i'),
   };
-  const deals = await dealService.getDealList(filter);
-  // TODO: sending empty array as lender's flow is not clear yet, asked the client waiting for his response.
-  return res.status(httpStatus.OK).send({ results: { deals, lenders: [] } });
+  const filterForLenders = {
+    lenderNameVisible: new RegExp(fields.searchValue, 'i'),
+  };
+  const deals = await dealService.getDealList(filterForDeal);
+  const lenders = await lendingInstitutionService.getLendingInstitutionList(filterForLenders);
+
+  return res.status(httpStatus.OK).send({ results: { deals, lenders } });
 });
