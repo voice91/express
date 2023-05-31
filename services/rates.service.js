47f4e961f145bc8f99d56d3274394270d72c2619
parag-radadiya <parag.180410107091@gmail.com>
2023-05-31 10:06:48 +0530

    Store_rates_in_db_and_find_average_for_lastWeek_and_lastMonth

--- services/rates.service.js
+++ services/rates.service.js
@@ -9,14 +9,17 @@ function calculateSumAndAverageByNames(records) {
   // eslint-disable-next-line no-plusplus
   for (let i = 0; i < records.length; i++) {
     const { name, current } = records[i];
+    // If the name already exists in the sumByNames object, update the sum and count
     if (sumByNames[name]) {
       sumByNames[name].sum += current;
       // eslint-disable-next-line no-plusplus
       sumByNames[name].count++;
     } else {
+      // If the name doesn't exist, initialize it with the current value and count of 1
       sumByNames[name] = { sum: current, count: 1 };
     }
   }
+  // Calculate average for each name
   const averageByNames = {};
   // eslint-disable-next-line guard-for-in,no-restricted-syntax
   for (const name in sumByNames) {
@@ -29,27 +32,36 @@ function calculateSumAndAverageByNames(records) {
 // eslint-disable-next-line import/prefer-default-export
 export async function ratesList(res, req, next) {
   try {
+    // Calculate the date for the last 24 hours
     const last24Hours = moment().subtract(24, 'hours').toDate();
+    // Find rates from the last 24 hours and sort them in descending order by createdAt
     const rates = await Rates.find({ createdAt: { $gte: last24Hours } }).sort({ createdAt: -1 });
 
+    // Calculate the date for the last week
     const lastWeekDate = moment().subtract(7, 'days').toDate();
+    // Find rates from the last week and sort them in descending order by createdAt
     const lastWeekRecord = await Rates.find({
       createdAt: {
         $gte: lastWeekDate,
       },
     }).sort({ createdAt: -1 });
 
+    // Calculate the sum and average for each name in the last week
     const lastWeek = calculateSumAndAverageByNames(lastWeekRecord);
 
+    // Calculate the date for the last month
     const lastMonthDate = moment().subtract(1, 'months').startOf('day').toDate();
+    // Find rates from the last month and sort them in descending order by createdAt
     const lastMonthRecord = await Rates.find({
       createdAt: {
         $gte: lastMonthDate,
       },
     }).sort({ createdAt: -1 });
 
+    // Calculate the sum and average for each name in the last month
     const lastMonth = calculateSumAndAverageByNames(lastMonthRecord);
 
+    // Combine rates with lastMonth and lastWeek data
     const filalResult = rates.map((item) => {
       return {
         ...item._doc,
--- utils/rates-cronJob.js
+++ utils/rates-cronJob.js
@@ -2,11 +2,12 @@ import axios from 'axios';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import cron from 'node-cron';
 import { Rates } from '../models';
+import config from '../config/config';
 
 // eslint-disable-next-line import/prefer-default-export
 export const storeRates = async () => {
   try {
-    const response = await axios.get('https://thefinancials.com/syndicated/PARALLEL/feed.json');
+    const response = await axios.get(config.getsRatesData);
     await Rates.create(Object.values(response.data));
   } catch (error) {
     console.error('Error occurred during the cron job:', error);
