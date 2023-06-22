a456e8d5e6b6adfe8b78d94b48479228f17c2efa
parag-radadiya <parag.180410107091@gmail.com>
2023-06-22 18:40:47 +0530

    refactor create function

--- controllers/advisor/dealSummary.controller.js
+++ controllers/advisor/dealSummary.controller.js
@@ -3,13 +3,13 @@ import { dealSummaryService } from '../../services';
 import { catchAsync } from '../../utils/catchAsync';
 
 // eslint-disable-next-line import/prefer-default-export
-export async function create(req, res) {
+export const create = catchAsync(async (req, res) => {
   const { body } = req;
   body.createdBy = req.user._id;
   body.updatedBy = req.user._id;
   const dealSummary = await dealSummaryService.createDealSummary(body);
   res.status(httpStatus.OK).send({ results: dealSummary });
-}
+});
 
 export const get = catchAsync(async (req, res) => {
   const { dealSummaryId } = req.params;
