f2106cdf67b073392b0a55dd5cb686d2226997d9
beladiyahardik7 <beladiyahardik7@gmail.com>
2023-06-27 20:34:43 +0530

    when there is no amount in minimum loan account field, then do not add validation and when there is loan amount, then check validation(< 100000)

--- models/lenderProgram.model.js
+++ models/lenderProgram.model.js
@@ -48,8 +48,8 @@ const LenderProgramSchema = new mongoose.Schema(
      * */
     minLoanSize: {
       type: Number,
-      min: 100000,
-      max: 1000000000,
+      min: 0,
+      max: 100000,
     },
     minLoanTag: { type: Number, default: 1 },
 
