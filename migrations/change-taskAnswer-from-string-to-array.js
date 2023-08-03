60872e0b4a14fc5ded61f8555ca71cf25999894f
parag-radadiya <parag.180410107091@gmail.com>
2023-08-03 18:45:25 +0530

    migrationFile commented.

--- migrations/change-taskAnswer-from-string-to-array.js
+++ migrations/change-taskAnswer-from-string-to-array.js
@@ -1,21 +1,21 @@
-const { asyncForEach } = require('../utils/common');
-
-module.exports = {
-  async up(db) {
-    // Connect to your MongoDB database
-    // Retrieve all documents from the Task collection with taskAnswer as a string
-    const Task = db.collection('Task');
-    const tasksToUpdateCursor = await Task.find({ taskAnswer: { $type: 'string' } });
-    const tasksToUpdate = await tasksToUpdateCursor.toArray();
-    await asyncForEach(tasksToUpdate, async (task) => {
-      await Task.updateOne(
-        { _id: task._id },
-        {
-          $set: {
-            taskAnswer: [{ answer: task.taskAnswer, receivedAt: new Date(), answeredBy: 'user' }],
-          },
-        }
-      );
-    });
-  },
-};
+// const { asyncForEach } = require('../utils/common');
+//
+// module.exports = {
+//   async up(db) {
+//     // Connect to your MongoDB database
+//     // Retrieve all documents from the Task collection with taskAnswer as a string
+//     const Task = db.collection('Task');
+//     const tasksToUpdateCursor = await Task.find({ taskAnswer: { $type: 'string' } });
+//     const tasksToUpdate = await tasksToUpdateCursor.toArray();
+//     await asyncForEach(tasksToUpdate, async (task) => {
+//       await Task.updateOne(
+//         { _id: task._id },
+//         {
+//           $set: {
+//             taskAnswer: [{ answer: task.taskAnswer, receivedAt: new Date(), answeredBy: 'user' }],
+//           },
+//         }
+//       );
+//     });
+//   },
+// };
