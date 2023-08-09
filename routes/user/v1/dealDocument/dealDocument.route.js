c54a45adbb37bc695591fe7fee221045186cae39
parag-radadiya <parag.180410107091@gmail.com>
2023-08-09 11:21:01 +0530

    fix role issue.

--- routes/user/v1/dealDocument/dealDocument.route.js
+++ routes/user/v1/dealDocument/dealDocument.route.js
@@ -29,7 +29,7 @@ router
    * createDealDocument updated flow
    * */
   .post(
-    auth('advisor'),
+    auth('user'),
     validate(dealDocumentValidation.createDealDocumentV2),
     checkAccessOfDeal,
     dealDocumentController.createV2
