96d9f0e464c027bc5cbaa58f7e08a58f21f8f51f
hardikbeladiya <beladiyahardik7@gmail.com>
2023-08-26 10:50:35 +0530

    Added the CI/CD pipeline for the staging.

---
+++ scripts/after_install.sh
@@ -0,0 +1,10 @@
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+cd lender-backend
+echo "installing yarn"
+npm install -g yarn
+echo "installing node modules"
+yarn install
+echo "creating build"
+yarn run build
---
+++ scripts/application_start.sh
@@ -0,0 +1,5 @@
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+cd lender-backend
+npm run start-pm2
