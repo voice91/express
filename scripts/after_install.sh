e674f99eda240653bda0cd221f22e662a1c73f35
hardikbeladiya <beladiyahardik7@gmail.com>
2023-08-26 12:21:34 +0530

    fix the CI/CD

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,7 +1,7 @@
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-cd /home/ubuntu/lender-backend
+cd /opt/lender-backend
 echo "installing yarn"
 npm install -g yarn
 echo "installing node modules"
--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,5 +1,5 @@
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-cd /home/ubuntu/lender-backend
+cd /opt/lender-backend
 npm run start-pm2-dev
