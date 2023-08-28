fe022b0a2c9779e2b56f96e16b84ab8bd2c7f133
Hardik Beladiya <beladiyahardik7@gmail.com>
2023-08-28 19:12:26 +0530

    Merge to Production. (#358)
    * fix the naming convention.
    
    * Production (#357)
    
    * Added the pipeline for the production.
    
    * Fix the issue of round of in the In-Place DY of dealmetrix field. (#347) (#348)
    
    * Fix the issue of round of in the In-Place DY of dealmetrix field. (#347)
    
    Co-authored-by: hardikbeladiya <beladiyahardik7@gmail.com>
    
    * Fix the issue of some deal not show even when we check all pages. (#349)
    
    Co-authored-by: hardikbeladiya <beladiyahardik7@gmail.com>
    
    ---------
    
    Co-authored-by: Bhavesh Beladiya <bhaveshbeladiyauwsb@gmail.com>
    
    * Merge to Production. (#356)
    
    * Fix the issue of round of in the In-Place DY of dealmetrix field. (#347)
    
    Co-authored-by: hardikbeladiya <beladiyahardik7@gmail.com>
    
    * Fix the issue of some deal not show even when we check all pages. (#349)
    
    Co-authored-by: hardikbeladiya <beladiyahardik7@gmail.com>
    
    * Allow null in the headquarter, website, description & logo & remove that field from DB. (#350)
    
    Co-authored-by: hardikbeladiya <beladiyahardik7@gmail.com>
    
    * When user send deal for the second time then the status does not change from New to Sent. (#351)
    
    Co-authored-by: hardikbeladiya <beladiyahardik7@gmail.com>
    
    * Change the flow of lender register remove verify email & directly set it. (#352)
    
    Co-authored-by: hardikbeladiya <beladiyahardik7@gmail.com>
    
    * allow null values for square footage, unit count and occupancy in post method of deal api. (#353)
    
    Co-authored-by: hardikbeladiya <beladiyahardik7@gmail.com>
    
    * Save & send token in the register API's response. (#354)
    
    Co-authored-by: hardikbeladiya <beladiyahardik7@gmail.com>
    
    ---------
    
    Co-authored-by: Bhavesh Beladiya <bhaveshbeladiyauwsb@gmail.com>
    
    ---------
    
    Co-authored-by: Bhavesh Beladiya <bhaveshbeladiyauwsb@gmail.com>
    
    ---------
    
    Co-authored-by: Bhavesh Beladiya <bhaveshbeladiyauwsb@gmail.com>

---
+++ routes/common/webhook/webhook.route.js
@@ -0,0 +1,7 @@
+import express from 'express';
+import { webhookController } from 'controllers/common';
+
+const router = express();
+
+router.post('/post-mark', webhookController.processEmailMessage);
+module.exports = router;
--- routes/common/wehbook/wehbook.route.js
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-import { wehbookController } from 'controllers/common';
-
-const router = express();
-
-router.post('/', wehbookController.list);
-module.exports = router;
