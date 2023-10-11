03bb7945a4636bbc5aa595beb28a6258a7376a6f
hardikbeladiya <beladiyahardik7@gmail.com>
2023-10-11 19:47:56 +0530

    Ignore the webhook path from encrypt/decrypt functionality.

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ app.use(helmet());
 app.use(express.json({ limit: '50mb' }));
 // decrypt the req body and parse to Json format.
 app.use((req, res, next) => {
-  if (config.dataEncryption && !isEmpty(req.body)) {
+  if (config.dataEncryption && !isEmpty(req.body) && !req.url.includes('/v1/webhook')) {
     if (!req.body.reqBody) {
       throw new ApiError(httpStatus.BAD_REQUEST, 'Request body must be encrypted');
     }
