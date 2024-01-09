d202278c666d8dc96ae431582e5877f0cd03ad21
Romil <romil@matlabinfotech.com>
2024-01-09 18:35:07 +0530

    comment on swagger code.

--- routes/common/docs/swagger.route.js
+++ routes/common/docs/swagger.route.js
@@ -1,8 +1,8 @@
 import express from 'express';
-import swaggerUi from 'swagger-ui-express';
-import YAML from 'yamljs';
+// import swaggerUi from 'swagger-ui-express';
+// import YAML from 'yamljs';
 
-const swaggerDocument = YAML.load('docs/opencollection.yml');
+// const swaggerDocument = YAML.load('docs/opencollection.yml');
 const router = express.Router();
-router.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+// router.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 module.exports = router;
